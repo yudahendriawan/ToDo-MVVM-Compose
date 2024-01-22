502a08f93e4a6d9b990ce7325bec247c92d5a426
yudahendriawan <yudahendriawan007@gmail.com>
2024-01-22 23:14:28 +1000

    setup dagger-hilt

---
+++ app/src/main/java/com/example/todo_mvvm_compose/ToDoApplication.kt
@@ -0,0 +1,9 @@
+package com.example.todo_mvvm_compose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ToDoApplication: Application() {
+
+}
--- app/src/main/java/com/example/todo_mvvm_compose/data/ToDoDatabase.kt
+++ app/src/main/java/com/example/todo_mvvm_compose/data/ToDoDatabase.kt
@@ -1,10 +1,11 @@
 package com.example.todo_mvvm_compose.data
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.example.todo_mvvm_compose.data.model.ToDoTask
 
 @Database(entities = [ToDoTask::class], version = 1, exportSchema = false)
-abstract class ToDoDatabase {
+abstract class ToDoDatabase: RoomDatabase() {
 
     abstract fun todoDao(): ToDoDao
 
